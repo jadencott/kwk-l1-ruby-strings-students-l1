b803b689ef18947a48724be3cf0ae90f5386c7ad
Jaden <jcottorone21405@gmail.com>
2018-06-13 20:25:19 +0000

    automatically backed up by learn

---
+++ names_hello.rb
@@ -0,0 +1,18 @@
+def say_hello
+  your name = "Charlie"
+  puts "Hello #{your_name}!"
+ end
+
+def say_hello_two
+ your name = "Randi"
+  puts "Hello #{your_name}!"
+end 
+
+def say_hello_three
+   your_name ="Gabriella"
+   puts "Hello #{your_name}!"
+ end
+
+say_hello
+say_hello_two
+say_hello_three
